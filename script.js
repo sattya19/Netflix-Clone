719a6289ac6f65ea4a7a88ecf1fee3a2245e1b61
sattya19 <sattyasharma195@gmail.com>
2023-08-12 14:51:43 +0530

    edited homePage

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function login() {
 function homePage() {
     window.location.href = "homepage.html";
 }
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselSlides = document.querySelectorAll('.carousel-slide');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    if (index < 0) {
+        index = carouselSlides.length - 1;
+    } else if (index >= carouselSlides.length) {
+        index = 0;
+    }
+
+    carouselInner.style.transform = `translateX(-${index * 100}%)`;
+    currentIndex = index;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
+
+// Initial slide
+showSlide(currentIndex);
